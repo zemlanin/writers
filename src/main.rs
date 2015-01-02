9aeabe27c2dc22cfeff4d520a371c5e3a7dab230
Anton Verinov <git@zemlan.in>
2015-01-02 12:07:00 +0200

    refine opts parsing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use std::os;
 use std::io::{fs, USER_DIR};
 
 fn main() {
-    let [input_path, output_path] = match lib::shell_args().as_slice() {
-        [Ok(ref path_1), Ok(ref path_2)] => [path_1, path_2],
+    let (input_path, output_path) = match lib::shell_args() {
+        Ok((path_1, path_2)) => (path_1, path_2),
         _ => {
-            println!("Error while parsing args");
+            println!("Error while parsing opts");
             return
         }
     };
 
     println!("{} {}", input_path.display(), output_path.display());
 
-    let contents = match fs::readdir(input_path) {
+    let contents = match fs::readdir(&input_path) {
         Ok(result) => result,
         Err(_) => {
             println!("Enter input directory as a first argument");
@@ -20,32 +19,43 @@ fn main() {
         }
     };
 
-    fs::mkdir_recursive(output_path, USER_DIR);
+    match fs::mkdir_recursive(&output_path, USER_DIR) {
+        Err(msg) => {
+            println!("{}", msg);
+            return
+        },
+        _ => {}
+    };
 
-    for inner_path in contents.iter() {
-        let absolute_path = os::make_absolute(inner_path).unwrap();
+    for absolute_path in contents.iter() {
         println!("{}", absolute_path.display());
+
+        if absolute_path.display().to_string().ends_with("html") {
+            println!("tmpl!");
+        };
     }
 }
 
 mod lib {
     use std::os;
-    use std::io::IoResult;
+    use std::io;
 
-    pub fn shell_args() -> Vec<IoResult<Path>> {
+    fn parse_path_opt(position: uint, default_value: String) -> io::IoResult<Path> {
         let args = os::args();
-        let input_path = if args.len() > 1 {
-            os::make_absolute(&Path::new(&args[1]))
+        os::make_absolute(&if args.len() > position {
+            Path::new(&args[position])
         } else {
-            os::make_absolute(&Path::new("../input/")) // because of sublime text's cargo build
-        };
+            Path::new(default_value) // because of sublime text's cargo build
+        })
+    }
 
-        let output_path = if args.len() > 2 {
-            os::make_absolute(&Path::new(&args[2]))
-        } else {
-            os::make_absolute(&Path::new("../output/")) // because of sublime text's cargo build
-        };
+    pub fn shell_args() -> io::IoResult<(Path, Path)> {
+        let input_arg = parse_path_opt(1, "../input/".to_string());
+        let output_arg = parse_path_opt(2, "../output/".to_string());
 
-        return vec!(input_path, output_path)
+        match (input_arg, output_arg) {
+            (Ok(input_path), Ok(output_path)) => Ok((input_path, output_path)),
+            (Err(msg), _) | (_, Err(msg)) => Err(msg)
+        }
     }
 }
